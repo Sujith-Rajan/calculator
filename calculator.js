569a615ab4df890d1d55bd7b179adf15ff3148d0
Sujith-Rajan <sujithrajan223@gmail.com>
2023-08-16 18:51:03 +0530

    ui design completed

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons =document.querySelectorAll(".operation")
+const clearButton =document.querySelector("#c");
+const resetButton =document.querySelector("#ac");
+const equalTo=document.querySelector("#equal");
+const display =document.querySelector("#display");
+
+let currentDisplay= "";
+
